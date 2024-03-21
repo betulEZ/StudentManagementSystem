ee99f1ab9f3d242f6226be22822bd8da4e98c7e4
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-03-21 17:33:50 +0100

    add update and delete in StudentList

--- frontend/src/service/StudentService.ts
+++ frontend/src/service/StudentService.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
+import {Student} from "../types/Student.ts";
 
 export default class StudentService{
     getAllStudents() {
         return axios.get('/api/students');
+    }
+    getStudentsById(id: string) {
+        return axios.get(`/api/students/${id}`);
+    }
+
+    addStudent(student: Student) {
+        return axios.post('/api/Students', student);
+    }
+    updateStudent(id:string, student: Student) {
+        return axios.put(`/api/students/${id}`,student);
+    }
+    deleteStudents(id: string) {
+        return axios.delete(`/api/students/${id}`);
     }
 
 }
