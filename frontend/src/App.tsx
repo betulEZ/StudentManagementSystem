13932c46e572728b07a9a4fe821e4ae5f8506400
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-03-27 16:31:10 +0100

    update set

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import StudentHomePage from "./studentPage/StudentHomePage.tsx";
 import {useState} from "react";
 
 export default function App() {
-    const [studentId] = useState<string>("1");
+    const [studentId,setStudentId] = useState<string>("1");
 
     return (
     <>
