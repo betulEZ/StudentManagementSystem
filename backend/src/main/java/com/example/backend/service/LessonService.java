bb3520cd45fb3362d268d2ed568019cf6ab345ca
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-03-27 12:46:10 +0100

    delete unused Import

--- backend/src/main/java/com/example/backend/service/LessonService.java
+++ backend/src/main/java/com/example/backend/service/LessonService.java
@@ -5,14 +5,12 @@ import com.example.backend.model.LessonDTO;
 import com.example.backend.model.Student;
 import com.example.backend.repository.LessonRepository;
 import com.example.backend.repository.StudentRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 
