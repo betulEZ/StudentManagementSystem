add800b45f6b88a5dd0cc0876d97b943a3b10dcd
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-04-03 17:46:10 +0200

    delete unnecessary import

--- backend/src/main/java/com/example/backend/controller/StudentController.java
+++ backend/src/main/java/com/example/backend/controller/StudentController.java
@@ -1,6 +1,5 @@
 package com.example.backend.controller;
 
-import com.example.backend.model.Lesson;
 import com.example.backend.model.LessonDTO;
 import com.example.backend.model.Student;
 import com.example.backend.model.StudentDTO;
